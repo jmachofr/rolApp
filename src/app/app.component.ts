9d19ed77e3d46be2812fca88919957b7938a6e85
Julián Macho Fraga <machofraga.julian@hotmail.com>
2018-09-04 09:46:23 -0300

    Revert "asd"
    This reverts commit 5cf5a6b8d3cbae05d7385bee2141c51961ea5a38.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
-import { DatosPage } from "../pages/datos/datos";
 
 @Component({
   templateUrl: 'app.html'
@@ -23,8 +22,7 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage },
-      { title: 'Datos', component: DatosPage},
+      { title: 'List', component: ListPage }
     ];
 
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
-import { DatosPage } from '../pages/datos/datos';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
