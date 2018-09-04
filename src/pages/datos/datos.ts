bb4cc00c36839dbbe3a3daf5fa703da535a35ee2
Julián Macho Fraga <machofraga.julian@hotmail.com>
2018-09-04 02:30:59 -0300

    name change to DatosPage
    name change to DatosPage

--- src/pages/datos/datos.ts
+++ src/pages/datos/datos.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 @Component({
-  selector: 'page-list',
-  templateUrl: 'list.html'
+  selector: 'page-datos',
+  templateUrl: 'datos.html'
 })
-export class ListPage {
+export class DatosPage {
   selectedItem: any;
   icons: string[];
   items: Array<{title: string, note: string, icon: string}>;
@@ -30,7 +30,7 @@ export class ListPage {
 
   itemTapped(event, item) {
     // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ListPage, {
+    this.navCtrl.push(DatosPage, {
       item: item
     });
   }
