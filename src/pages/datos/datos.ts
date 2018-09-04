5cf5a6b8d3cbae05d7385bee2141c51961ea5a38
Julián Macho Fraga <machofraga.julian@hotmail.com>
2018-09-04 09:46:02 -0300

    asd
    asd

---
+++ src/pages/datos/datos.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-datos',
+  templateUrl: 'datos.html'
+})
+export class DatosPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
